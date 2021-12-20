ffa0dec3c3fb2c2a13c74221f7d1ecc20d5c5bad
fazal452 <64503906+fazal452@users.noreply.github.com>
2021-12-19 23:29:57 -0800

    Part 2

--- day_4.cpp
+++ day_4.cpp
@@ -15,7 +15,7 @@ struct bingo_number{
 };
 
 struct sheet{
-    
+
     bingo_number vals[5][5];
 
 };
@@ -30,7 +30,7 @@ struct winner{
 int main() {
 
     winner currwinner;
-    currwinner.roundOfWin = 9999;
+    currwinner.roundOfWin = 9;
 
     int arr[] = {1,76,38,96,62,41,27,33,4,2,94,15,89,25,66,14,30,0,71,21,48,44,87,73,60,50,77,45,29,18,5,99,65,16,93,95,37,3,52,32,46,80,98,63,92,24,35,55,12,81,51,17,70,78,61,91,54,8,72,40,74,68,75,67,39,64,10,53,9,31,6,7,47,42,90,20,19,36,22,43,58,28,79,86,57,49,83,84,97,11,85,26,69,23,59,82,88,34,56,13};
     int size = (sizeof(arr)/sizeof(arr[0]));
@@ -133,13 +133,13 @@ int main() {
                             hand.roundOfWin = i;
                             hand.score = unmarked * bingo;
 
-                            if (hand.roundOfWin < currwinner.roundOfWin){
+                            if (hand.roundOfWin > currwinner.roundOfWin){
                                 currwinner = hand;
                             }
 
                             //cout << unmarked * bingo << endl;
 
-
+                            break;
                         }
 
                     }
